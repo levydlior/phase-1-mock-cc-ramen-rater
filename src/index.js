097fd5f1b75f88423e230212672e0eeaf4923699
Lior <levydlior@gmail.com>
2022-05-24 11:10:13 -0600

    all the basic requirments completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,74 @@
 // write your code here
+
+const ramenURL = "http://localhost:3000/ramens";
+const ramenMenu = document.querySelector("#ramen-menu");
+const ramenDetails = document.querySelector("#ramen-detail");
+const newRamenForm = document.querySelector("#new-ramen");
+
+fetch(ramenURL)
+  .then((response) => response.json())
+  .then((ramens) =>
+    ramens.forEach((ramen) => {
+      renderRamenrsToPage(ramen);
+    })
+  );
+
+function renderRamenrsToPage(ramen) {
+  const ramenImg = document.createElement("img");
+  const ramenName = ramen.name;
+  const ramenResturant = ramen.restaurant;
+  const ramenRating = ramen.rating;
+  const ramenComment = ramen.comment;
+  const ramenId = ramen.id;
+  ramenImg.src = ramen.image;
+  ramenImg.alt = ramenName;
+  ramenImg.id =  ramenId
+
+  ramenImg.addEventListener("click", () => {
+    const h2 = document.querySelector("#ramen-name");
+    const h3 = document.querySelector("#resturant-name");
+    const pComment = document.querySelector("#comment-display");
+    const spanRatin = document.querySelector("#rating-display");
+    const detailsIMg = document.querySelector("#detailsImage");
+
+    h2.textContent = ramenName;
+    h3.textContent = ramenResturant;
+    pComment.textContent = ramenComment;
+    spanRatin.textContent = ramenRating;
+    detailsIMg.src = ramen.image;
+  });
+
+  ramenMenu.append(ramenImg);
+}
+
+newRamenForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newRamenFormData = new FormData(e.target);
+
+  const name = newRamenFormData.get("name");
+  const restaurant = newRamenFormData.get("restaurant");
+  const image = newRamenFormData.get("image");
+  const rating = newRamenFormData.get("rating");
+  const comment = newRamenFormData.get("new-comment");
+
+  const newRamenObject = {
+    name,
+    restaurant,
+    image,
+    rating,
+    comment,
+  };
+
+  fetch(ramenURL, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(newRamenObject),
+  })
+    .then((response) => response.json())
+    .then((updatedRamens) => {
+      renderRamenrsToPage(updatedRamens);
+    });
+});
